9d17643da79c24c85a02620665c849ac1865f6b9
Denis Isaev / Денис Исаев <sr_relikt@mail.ru>
2021-02-14 12:55:21 +0300

    add meta tags

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,14 @@
+import Head from 'next/head'
+
 // Get page - /posts
 export default function Posts() {
-  return <h1>Posts page</h1>;
+  return (
+    <>
+      <Head>
+          <title>NextJS App | Posts</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      <h1>Posts page</h1>
+    </>
+  );
 }
